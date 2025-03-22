00cd221a69692f491bc3eb52c3d6f6491ca76135
melyLaur <melissanoemi.laurent@free.fr>
2025-03-22 18:01:21 +0100

    deploy stage

--- src/main/java/fr/esgi/poc_pipelines/CustomController.java
+++ src/main/java/fr/esgi/poc_pipelines/CustomController.java
@@ -1,14 +1,18 @@
 package fr.esgi.poc_pipelines;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomController {
 
+    @Value("${test}")
+    private String test;
+
     @GetMapping("/")
     public String up() {
-        return "up";
+        return "up " + test;
     }
 
     @GetMapping("hello")
