08ab847e3d0f8cf1d4fc99639db6feb981d2bc9b
melyLaur <melissanoemi.laurent@free.fr>
2025-04-01 23:09:38 +0200

    test

---
+++ src/main/java/fr/esgi/poc_pipelines/CustomService.java
@@ -0,0 +1,17 @@
+package fr.esgi.poc_pipelines;
+
+
+/**
+ * Custom class with a method test which return "test"
+ */
+public class CustomService {
+    /**
+     * it is a test
+     * @param name : it is the name
+     * @return test
+     */
+    public String test(String name) {
+        System.out.println(name);
+        return "test";
+    }
+}
